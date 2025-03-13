0c432a3e0a0c28e2fabe97353015c2286e406829
Omarhudaib <omarhudaib.it@gmail.com>
2025-03-13 23:50:01 +0300

    add url

--- hr front sys/hr/src/api.js
+++ hr front sys/hr/src/api.js
@@ -2,7 +2,7 @@
   import axios from 'axios';
   const companyCode = JSON.parse(localStorage.getItem('company'))?.company_code;
   const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // قم بتغيير هذا حسب خادمك
+    baseURL: 'https://newhrsys-production.up.railway.app/api', // قم بتغيير هذا حسب خادمك
     headers: {"company_code":companyCode
 
     },
